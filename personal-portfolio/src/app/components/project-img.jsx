edd2801d60894e22b850f5f11c74987e46e376b5
unknown <devaabijo@gmail.com>
2024-08-05 18:43:23 +0530

    addded project1

--- personal-portfolio/src/app/components/project-img.jsx
+++ personal-portfolio/src/app/components/project-img.jsx
@@ -1,22 +1,55 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
-const ProjectImg = () => {
+const ProjectImg = ({ project }) => {
+
+  const projectimg = [
+    {
+      id: 1,
+      imgurl: ['cm1', 'cm2', 'cm3', 'cm4', 'cm5']
+    },
+    {
+      id: 2,
+      imgurl: ['em1', 'em2', 'em3', 'em4', 'em5']
+    },
+    {
+      id: 3,
+      imgurl: ['sd1', 'sd2', 'sd3', 'sd4', 'sd5']
+    }
+  ]
+  const [imgid, setimgId] = useState(0)
+
+  const changeimg = (direction) => {
+    if (!direction && imgid == 0)
+    {
+      setimgId(4)
+    } else if (!direction)
+    {
+      setimgId(imgid - 1)
+    } else if (direction && imgid == 4)
+    {
+      setimgId(0)
+    } else
+    {
+      setimgId(imgid + 1)
+    }
+  }
 
 
-
+  const imageUrl = projectimg[project - 1]?.imgurl[imgid];
 
   return (
     <div className='h-full w-full '>
-    <div className='h-[90%] w-full p-5'>
-        <div className='border-2 border-[#764fa4] h-full w-full'></div>
+      <div className='h-[90%] w-full p-5'>
+        <div className='border-2 border-[#764fa4] h-full w-full bg-center bg-cover' style={{ backgroundImage: `url('./assets/${imageUrl}.png')` }}> </div>
+      </div>
+      <div className='h-[10%] w-full flex px-5 justify-between'>
+        <button onClick={() => changeimg(0)} className=' h-[80%] w-32 bg-[#fad964] flex rounded-full justify-center items-center text-xl font-semibold text-black font-mono border-2 border-[#764fa4]'> Previous</button>
+        <button onClick={() => changeimg(1)} className='h-[80%] w-32 bg-[#fdda5f] flex rounded-full justify-center items-center text-xl font-semibold text-black font-mono border-2 border-[#764fa4]'> Next</button>
+      </div>
     </div>
-    <div className='h-[10%] w-full flex px-5 justify-between'>
-        <div className='h-[80%] w-32 bg-[#fdda5f] flex rounded-full justify-center items-center text-xl font-semibold text-black font-mono border-2 border-[#764fa4]'> Next</div>
-        <div className=' h-[80%] w-32 bg-[#fad964] flex rounded-full justify-center items-center text-xl font-semibold text-black font-mono border-2 border-[#764fa4]'> Previous</div>
-    </div>
-</div>
   )
-  
+
 }
 
 export default ProjectImg
--- personal-portfolio/src/app/components/project.jsx
+++ personal-portfolio/src/app/components/project.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import Logoslider from './logoslider'
 import ProjectImg from "./project-img"
 const Project = () => {
+
+const [project,setProject] =useState(1);
+
+   const changeProject =(projectid)=>{
+        setProject(projectid)
+    }
+
     return (
         <div className='relative'>
         <div className='bg-[#a67cfe]  '>
@@ -9,16 +18,16 @@ const Project = () => {
                 <div className='h-full w-fit flex flex-col justify-center items-start   ' >
                     <p className='text-5xl font-mono text-black font-semibold'>My Projects...</p>
                     <div className='space-y-10 pt-10 w-full'>
-                       <div className='flex items-center h-24 w-full justify-center border-[#764fa4] rounded-full border-2 bg-[#d8b8fb] hover:shadow-[#d8b8fb] hover:shadow-2xl'> <p className='text-2xl font-mono text-black '>Campus Maintence</p>   </div>
-                       <div className='flex items-center  h-24  w-full justify-center border-[#764fa4] rounded-full border-2 bg-[#d8b8fb] hover:shadow-[#d8b8fb] hover:shadow-2xl'> <p className='text-2xl font-mono text-black'>Event Management</p> </div>
-                       <div className='flex items-center h-24 w-full  justify-center border-[#764fa4] rounded-full border-2 bg-[#d8b8fb] hover:shadow-[#d8b8fb] hover:shadow-2xl'> <p className='text-2xl font-mono text-wrap text-black w-80 text-center'>Student attendence System</p> </div>
+                       <button onClick={()=>changeProject(1)} className='flex items-center h-24 w-full justify-center border-[#764fa4] rounded-full border-2 bg-[#d8b8fb] hover:shadow-[#d8b8fb] hover:shadow-2xl'> <p className='text-2xl font-mono text-black '>Campus Maintence</p>   </button>
+                       <button onClick={()=>changeProject(2)} className='flex items-center  h-24  w-full justify-center border-[#764fa4] rounded-full border-2 bg-[#d8b8fb] hover:shadow-[#d8b8fb] hover:shadow-2xl'> <p className='text-2xl font-mono text-black'>Event Management</p> </button>
+                       <button onClick={()=>changeProject(3)} className='flex items-center h-24 w-full  justify-center border-[#764fa4] rounded-full border-2 bg-[#d8b8fb] hover:shadow-[#d8b8fb] hover:shadow-2xl'> <p className='text-2xl font-mono text-wrap text-black w-80 text-center'>Student attendence System</p> </button>
 
                     </div>
                 </div>
 
                 <div className='border-4 border-[#764fa4] h-full w-[70%] rounded-lg flex z-20 bg-[#d8b8fb]' >
                     <div className='h-full w-32 bg-[#120a1c] rounded-sm' ><Logoslider></Logoslider></div>
-<ProjectImg></ProjectImg>
+<ProjectImg project={project}></ProjectImg>
                 </div>
 
 
