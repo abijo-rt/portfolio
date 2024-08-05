94cd2f546e7425c3c35762e30270e2b3f9c3c060
unknown <devaabijo@gmail.com>
2024-08-05 10:06:30 +0530

    Removed error

--- personal-portfolio/src/app/components/about.jsx
+++ personal-portfolio/src/app/components/about.jsx
@@ -5,13 +5,13 @@ const About = () => {
          <div className=' flex h-screen w-screen items-center justify-center   px-32'>
          <div className='flex flex-col h-3/4 w-1/4 items-start justify-start '>
          <div className='text-black font-mono text-6xl '>About</div><br/>
-         <div className='font-mono text-4xl '>"The only way to do great work is to love what you do.."</div>
+         <div className='font-mono text-4xl '>&quot;The only way to do great work is to love what you do..&quot;</div>
          </div>
 
          <div className='flex  h-3/4 w-3/4 items-center justify-center pl-32'>
 
 <div className='border-[#764fa4] border-2 h-full w-full bg-[#d9ffb5] p-10 text-black font-medium font-mono rounded-lg shadow-md shadow-[#d9ffb5]'>
-  <p className='overflow-hidden h-fit w-full text-justify '>"Hello! I'm Deva Abijo R T, a passionate Full Stack Developer with a love for creating innovative solutions. Currently exploring the dynamic world of web development and continuously learning new technologies."</p>
+  <p className='overflow-hidden h-fit w-full text-justify '>&quot;Hello! I&apos;m Deva Abijo R T, a passionate Full Stack Developer with a love for creating innovative solutions. Currently exploring the dynamic world of web development and continuously learning new technologies.&quot;</p>
 <br/>
 <br/>
 <p className=' flex items-center '><img src='./assets/col.svg ' className='size-8 ml-10 hover:animate-pulse'></img><p className='ml-10'>Bannari Amman Institute Of technology</p></p>
